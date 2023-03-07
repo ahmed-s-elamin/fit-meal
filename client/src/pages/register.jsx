eeeb292cd874e63f73cdd5cd8041cdf2272740b3
eddyiiz <eddyxsayf@gmail.com>
2023-03-07 16:28:33 +0200

    added toast for register page

--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import axios from "axios";
 
 export const Register = () => {
