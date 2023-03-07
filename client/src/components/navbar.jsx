8f3341af5b636fd3b30e6bf72f708bcd0fd5f670
eddyiiz <eddyxsayf@gmail.com>
2023-03-07 16:21:51 +0200

    added logout

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,13 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { toast } from "react-toastify";
 
 export const Navbar = () => {
+  const [cookies, setCookies] = useCookies(["access_token"]);
+
+  const navigate = useNavigate();
+
+  const logout = () => {
+    setCookies("access_token", "");
+
+    window.localStorage.removeItem("userID");
+    toast.success("logged out");
+    navigate("/login");
+  };
   return (
     <div className="navbar">
       <Link to="/"> Home</Link>
       <Link to="/create-recipe"> Create Recipe</Link>
       <Link to="/saved-recipes"> Saved Recipes</Link>
-      <Link to="/login"> Login</Link>
-      <Link to="/register"> register</Link>
+      <Link to="/register"> Register</Link>
+      {!cookies.access_token ? (
+        <Link to="/login"> Login</Link>
+      ) : (
+        <button onClick={logout}>Logout</button>
+      )}
     </div>
   );
 };
