ed8e419863f4fa838cadb18d1cd44cbddf8a2cfa
eddyiiz <eddyxsayf@gmail.com>
2023-03-07 15:55:12 +0200

    handle login

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,12 +1,38 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import axios from "axios";
 
 export const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  const [_, setCookies] = useCookies(["access_token"]);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:3030/auth/login", {
+        username,
+        password,
+      });
+
+      setCookies("access_token", response.data.token);
+      window.localStorage.setItem("userID", response.data.userID);
+
+      alert("successfully logged in!");
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="auth-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Login</h2>
         <div className="form-group">
           <label htmlFor="username"> Username: </label>
@@ -20,7 +46,7 @@ export const Login = () => {
         <div className="form-group">
           <label htmlFor="password"> Password: </label>
           <input
-            type="text"
+            type="password"
             id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
