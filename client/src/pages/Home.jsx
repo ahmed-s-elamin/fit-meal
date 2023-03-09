0344b7f2a423805370799071dc31d115e87dfa3a
eddyiiz <eddyxsayf@gmail.com>
2023-03-09 11:26:05 +0200

    fetch recipes on home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,39 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export const Home = () => {
-  return <div>Home</div>;
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
+
+  const fetchRecipes = async () => {
+    try {
+      const response = await axios.get("http://localhost:3030/recipes");
+      setRecipes(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Recipes</h1>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe._id}>
+            <div>
+              <h1>{recipe.name}</h1>
+            </div>
+            <div className="instructions">
+              <p>{recipe.instructions}</p>
+            </div>
+            <img src="{recipe.imageUrl}" alt="{recipe.name}" />
+            <p> Cooking Time: {recipe.cookingTime} (minutes)</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
