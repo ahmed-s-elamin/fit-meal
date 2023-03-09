d86a520b3c0d1b7ab206f577b1b8f05380ad0a83
eddyiiz <eddyxsayf@gmail.com>
2023-03-09 12:55:15 +0200

    done

--- client/src/pages/saved-recipes.jsx
+++ client/src/pages/saved-recipes.jsx
@@ -1,3 +1,45 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useGetUserID } from "../hooks/useGetUserID";
+
 export const SavedRecipes = () => {
-  return <div>SavedRecipes</div>;
+  const [savedRecipes, setSavedRecipes] = useState([]);
+
+  const userID = useGetUserID();
+
+  useEffect(() => {
+    fetchSavedRecipes();
+  }, []);
+
+  const fetchSavedRecipes = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3030/recipes/savedRecipes/${userID}`
+      );
+      setSavedRecipes(response.data.savedRecipes);
+      //console.log(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Saved Recipes</h1>
+      <ul>
+        {savedRecipes.map((recipe) => (
+          <li key={recipe._id}>
+            <div>
+              <h1>{recipe.name}</h1>
+            </div>
+            <div className="instructions">
+              <p>{recipe.instructions}</p>
+            </div>
+            <img src={recipe.imageUrl} alt={recipe.name} />
+            <p> Cooking Time: {recipe.cookingTime} (minutes)</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
